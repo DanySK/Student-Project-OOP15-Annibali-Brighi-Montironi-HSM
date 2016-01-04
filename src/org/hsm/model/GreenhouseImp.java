0627f8c67ca016e126cfa74d540319897f767062
Andrea Montironi <andrea.montironi@studio.unibo.it>
2016-01-04 19:58:43 +0100

    write addPlant, delPlant and delPlants in GreenhouseImp. beta version

--- src/org/hsm/model/GreenhouseImp.java
+++ src/org/hsm/model/GreenhouseImp.java
@@ -14,11 +14,13 @@ public class GreenhouseImp implements GreenHouse {
      * This method add some plants to the Greenhouse.
      * @param n
      *      number of plants
-     * @param namePlant
-     *      name of the plant
+     * @param plant
+     *      an object of type Plant
      */
-    protected void addPlants(final int n, final String namePlant) {
-       
+    protected void addPlants(final int n, final Plant plant) {
+       for (int i = 0; i < n; i++) {
+           lp.add(plant);
+       }
     }
     
     /**
@@ -27,8 +29,13 @@ public class GreenhouseImp implements GreenHouse {
      * @param id
      *            Identifier for the plant
      */
-    void delPlant(int id){
-        
+    //TODO check this method (equal()???)
+    protected void delPlant(final int id) {
+        for (final Plant i : lp) {
+            if (i.getID() == id) {
+                lp.remove(i);
+            }
+        }
     }
 
     /**
@@ -37,9 +44,45 @@ public class GreenhouseImp implements GreenHouse {
      * @param plant
      *            type of plant to be delete
      */
-    void delPLants(Plant plant){
+   protected void delPLants(final Plant plant) {
+       for (final Plant i : lp) {
+           if (i.getBotanicalName().equals(plant.getBotanicalName())) {
+               lp.remove(i);
+           }
+       }
+    }
+    
+    /**
+     * Create a new type of plant for the database.
+     * 
+     * @param name
+     *            the name of the plant
+     * @param id
+     *            the identifier of the plant
+     * @param botanicalName
+     *            the scientific name for the plant
+     * @param ph
+     *            optimal ph for the plant
+     * @param brightness
+     *            optimal brightness for the plant
+     * @param conductibility
+     *            optimal terrain conductivity for the plant
+     * @param optimalGrowthTime
+     *            optimal Growth time for the plant
+     * @param temperature
+     *            optimal temperature for the plant
+     * @param life
+     *            the life of the plant
+     * @param size
+     *            the space occupied from the plant in the greenhouse
+     * @param cost
+     *            the cost in euro of the plant
+     */
+    void createNewPlant(String name, int id, String botanicalName, int ph, int brightness, int conductivity,
+            int optimalGrowthTime, int temperature, int life, int size, int cost){
         
     }
+
 
    
 }
