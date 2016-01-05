8c99c8cf95f33a9a2c8a0fafa1b8fa12c1593673
Andrea Montironi <andrea.montironi@studio.unibo.it>
2016-01-05 18:37:24 +0100

    removed createNewPlant method from GreenhouseImp

--- src/org/hsm/model/GreenHouse.java
+++ src/org/hsm/model/GreenHouse.java
@@ -4,6 +4,11 @@ package org.hsm.model;
  * interface for the communication between the model and the controller.
  */
 public interface GreenHouse {
-	
+   
+    void addPlants(final int n, final Plant plant);
+    
+    void delPlant(final int id);
+    
+    void delPLants(final Plant plant);
     
 }
--- src/org/hsm/model/GreenhouseImp.java
+++ src/org/hsm/model/GreenhouseImp.java
@@ -17,7 +17,7 @@ public class GreenhouseImp implements GreenHouse {
      * @param plant
      *      an object of type Plant
      */
-    protected void addPlants(final int n, final Plant plant) {
+    public void addPlants(final int n, final Plant plant) {
        for (int i = 0; i < n; i++) {
            lp.add(plant);
        }
@@ -30,7 +30,7 @@ public class GreenhouseImp implements GreenHouse {
      *            Identifier for the plant
      */
     //TODO check this method (equal()???)
-    protected void delPlant(final int id) {
+    public void delPlant(final int id) {
         for (final Plant i : lp) {
             if (i.getID() == id) {
                 lp.remove(i);
@@ -44,45 +44,12 @@ public class GreenhouseImp implements GreenHouse {
      * @param plant
      *            type of plant to be delete
      */
-   protected void delPLants(final Plant plant) {
+   public void delPLants(final Plant plant) {
        for (final Plant i : lp) {
            if (i.getBotanicalName().equals(plant.getBotanicalName())) {
                lp.remove(i);
            }
        }
     }
-    
-    /**
-     * Create a new type of plant for the database.
-     * 
-     * @param name
-     *            the name of the plant
-     * @param id
-     *            the identifier of the plant
-     * @param botanicalName
-     *            the scientific name for the plant
-     * @param ph
-     *            optimal ph for the plant
-     * @param brightness
-     *            optimal brightness for the plant
-     * @param conductibility
-     *            optimal terrain conductivity for the plant
-     * @param optimalGrowthTime
-     *            optimal Growth time for the plant
-     * @param temperature
-     *            optimal temperature for the plant
-     * @param life
-     *            the life of the plant
-     * @param size
-     *            the space occupied from the plant in the greenhouse
-     * @param cost
-     *            the cost in euro of the plant
-     */
-    void createNewPlant(String name, int id, String botanicalName, int ph, int brightness, int conductivity,
-            int optimalGrowthTime, int temperature, int life, int size, int cost){
-        
-    }
-
-
    
 }
