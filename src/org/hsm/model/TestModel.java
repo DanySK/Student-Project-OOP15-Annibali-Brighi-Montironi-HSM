2a94a9b2da5edec4ef2d8cf611b1e309e8258ad9
Andrea Montironi <andrea.montironi@studio.unibo.it>
2016-01-26 11:22:40 +0100

    fixed TestModel

--- src/org/hsm/model/TestModel.java
+++ src/org/hsm/model/TestModel.java
@@ -7,7 +7,7 @@ public class TestModel {
         final Database db = new DBplants();
         db.addPlantModel("ciccio", "pasticcio", 1, 2, 3, 4, 5, 6, 7);
 
-        final GreenHouse gn = new GreenhouseImp();
+        final GreenHouse gn = new GreenhouseImp(null, 0, 0);
 
        gn.addPlants(5, db.getPlantModel("pasticcio"), 12);
 
