90eedeade1c5483d93246d36cdf94ac4a3f6fd1a
Andrea Montironi <andrea.montironi@studio.unibo.it>
2016-02-01 16:41:21 +0100

    added serializable in IDmanager

--- src/org/hsm/model/IDmanager.java
+++ src/org/hsm/model/IDmanager.java
@@ -1,12 +1,18 @@
 package org.hsm.model;
 
+import java.io.Serializable;
+
 /**Class for manage the ID of the plants in the greenhouse.
  *
  *
  */
-public class IDmanager {
+public class IDmanager implements Serializable {
 
-	private int id;
+	/**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    private int id;
 
 	/**This method increment the id field and return the new value.
 	 * @return id
