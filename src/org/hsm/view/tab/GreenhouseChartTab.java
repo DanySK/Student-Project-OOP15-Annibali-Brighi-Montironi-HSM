c35437517a3025e61f354a25f11e1acd45e91b23
Marco Brighi <marco.brighi4@studio.unibo.it>
2016-02-21 12:03:16 +0100

    Inserted greenhouse water chart.

--- src/org/hsm/view/tab/GreenhouseChartTab.java
+++ src/org/hsm/view/tab/GreenhouseChartTab.java
@@ -13,7 +13,10 @@ import org.hsm.controller.ControllerImpl;
 import org.hsm.view.gui.GUIComponent;
 import org.hsm.view.utility.GUIFactory;
 import org.hsm.view.utility.MyGUIFactory;
+import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.data.general.DefaultPieDataset;
 
 /**
@@ -32,10 +35,14 @@ public class GreenhouseChartTab implements GUIComponent {
         final GUIFactory factory = new MyGUIFactory();
         final JPanel buttonPanel = new JPanel();
         //buttons
-        final JButton numPlantChart = new JButton("Show Greenhouse Composition by Number");
-        final JButton spacePlantChart = new JButton("Show Greenhouse Composition by Occupied Space");
+        final JButton numPlantChart = new JButton("Composition by Number");
+        final JButton spacePlantChart = new JButton("Composition by Occupied Space");
+        final JButton waterConsuption = new JButton("Water Consuption");
+        final JButton growthTime = new JButton("Plants Growth Time");
         buttonPanel.add(numPlantChart);
         buttonPanel.add(spacePlantChart);
+        buttonPanel.add(waterConsuption);
+        buttonPanel.add(growthTime);
         //chart
         final ChartPanel chartPanel = new ChartPanel(null);
         final JScrollPane scrollPane = new JScrollPane(chartPanel);
@@ -54,6 +61,26 @@ public class GreenhouseChartTab implements GUIComponent {
                 dataset.setValue(elem.getKey(), elem.getValue());
             }
             chartPanel.setChart(factory.createPieChart(dataset));
+        });
+        waterConsuption.addActionListener(e -> {
+            final DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+            final int size = ControllerImpl.getController().getGreenhouse().getRealWaterConsuption().size();
+            for (int i = 0; i < size; ++i) {
+                dataset.addValue(ControllerImpl.getController().getGreenhouse().getRealWaterConsuption().get(i), 
+                                "Traditional Consuption", 
+                                Integer.toString(i));
+                dataset.addValue(ControllerImpl.getController().getGreenhouse().getSimulatedPlantGrow().get(i), 
+                                "Current Consuption", 
+                                Integer.toString(i));
+            }
+            chartPanel.setChart(ChartFactory.createLineChart("", 
+                                                             "",
+                                                             "", 
+                                                             dataset, 
+                                                             PlotOrientation.VERTICAL, 
+                                                             true, 
+                                                             true, 
+                                                             false));
         });
         this.panel.add(buttonPanel, BorderLayout.NORTH);
         this.panel.add(scrollPane, BorderLayout.CENTER);
