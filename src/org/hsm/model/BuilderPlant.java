b7c0e452d1ddaf3c092d975a3f3769270c33701b
Andrea Montironi <andrea.montironi@studio.unibo.it>
2016-02-23 16:20:42 +0100

    modified BuilderPlant

--- src/org/hsm/model/BuilderPlant.java
+++ src/org/hsm/model/BuilderPlant.java
@@ -122,10 +122,8 @@ public class BuilderPlant {
      * @throws IllegalArgumentException
      */
     public PlantModel build() throws IllegalArgumentException, IllegalStateException {
-        if(this.name.equals("") || this.botanicalName.equals("")){
-            throw new IllegalStateException();
-        }
-        if(this.size == 0 || this.ph == 0 || this.brightness == 0 || this.optimalGrowthTime == 0 || this.life == 0 || this.conductivity == 0 || this.optimalTemperature == 0){
+        if(this.name.equals("") || this.botanicalName.equals("") || 
+                (this.size & this.ph & this.brightness & this.optimalGrowthTime & this.life & this.conductivity & this.optimalTemperature) == 0){
             throw new IllegalArgumentException();
         }
         return new PlantModelImpl(this.name, this.botanicalName, this.ph, this.brightness, this.optimalGrowthTime,
