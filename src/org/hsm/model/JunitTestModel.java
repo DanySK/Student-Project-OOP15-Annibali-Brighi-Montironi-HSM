3ab84a57d72e2ce9aa89961a7e95dbf929f63a20
Andrea Montironi <andrea.montironi@studio.unibo.it>
2016-02-24 19:14:57 +0100

    added JunitTestModel

---
+++ src/org/hsm/model/JunitTestModel.java
@@ -0,0 +1,14 @@
+package org.hsm.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JunitTestModel {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
