1f7ba1687cb3e7894d354cc71b74ac5bc4bc2f1f
Andrea Montironi <andrea.montironi@studio.unibo.it>
2016-02-25 11:06:26 +0100

    modified JunitModelTest

--- src/org/hsm/model/JunitTestModel.java
+++ src/org/hsm/model/JunitTestModel.java
@@ -10,6 +10,7 @@ public class JunitTestModel {
 
     @Test
     public void test() {
+        final int TO_MS = 1000000;
         //create new database
         final Database db = new DatabaseImpl();
         //add 3 type of plants
@@ -28,26 +29,40 @@ public class JunitTestModel {
         int id = gh.addPlant(db.getPlantModel("botanical_uno"), 10);
         assertTrue(gh.getPlants().size() == 1);
 
-       
+
         //remove a plant
         gh.delPlant(id);
         assertTrue(gh.getPlants().size() == 0);
 
-        //add 100 plants of botanical_due
-        for(int i=0;i<100;i++){
-            gh.addPlant(db.getPlantModel("botanical_due"), 2);
+        //add 100 plants of botanical_due and test time
+        long time = System.nanoTime();
+        try{
+            for(int i=0;i<100;i++){
+                gh.addPlant(db.getPlantModel("botanical_due"), 2);
+            }
+        } catch (IllegalStateException e){
+            System.out.println("error: not enough free space");
         }
+        time = System.nanoTime() - time;
         assertTrue(gh.getPlants().size() == 100);
-        
+        System.out.println("time to add 100 elements: " + time / TO_MS + " ms");
+        assertTrue(time <= 100000000); // 1/10 of second
+
         //add 100 plants of botanical_uno
-        for(int i=0;i<100;i++){
-            gh.addPlant(db.getPlantModel("botanical_uno"), 2);
+        try{
+            for(int i=0;i<100;i++){
+                gh.addPlant(db.getPlantModel("botanical_uno"), 2);
+            }
+        } catch (IllegalStateException e){
+            System.out.println("error: not enough free space");   
         }
         assertTrue(gh.getPlants().size() == 200);
-        
+
         //remove all botanical_due
         gh.delPlants(db.getPlantModel("botanical_due"));
         assertTrue(gh.getPlants().size() == 100);
+
+        //try to add botanical_tre (previously removed)
         try{
             gh.addPlant(db.getPlantModel("botanical_tre"), 20);
         } catch (NullPointerException e){
@@ -55,9 +70,8 @@ public class JunitTestModel {
         } catch (IllegalStateException i){
             System.out.println("error: not enough free space");
         }
-        
-        System.out.println(gh.getFreeSize());
-        
+
+
     }
 
 }
